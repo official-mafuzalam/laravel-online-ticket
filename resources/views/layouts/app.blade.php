85269a4a55e9ce3ca35de1210007ab4ffa0abe85
Mafuz Alam <101239380+official-mafuzalam@users.noreply.github.com>
2023-07-29 17:31:24 +0600

    reg cancel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,11 +49,11 @@
                                 </li>
                             @endif
 
-                            @if (Route::has('register'))
+                            {{-- @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
-                            @endif
+                            @endif --}}
                         @else
                             @if (session('user.type', 'default') == 'admin')
                                 <li class="nav-item dropdown">
